bc7708a703a9ab15a36ab5fd5a06b483604c17c4
ocizek <ocizek@icloud.com>
2025-07-10 20:49:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: false, // 💥 důležité – vypne App Router
-  },
 };
 
 module.exports = nextConfig;
