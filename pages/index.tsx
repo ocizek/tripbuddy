8a6d0acf85f18bc1bacedeff8e047bae3e3a0830
ocizek <ocizek@icloud.com>
2025-07-10 20:58:50 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabase = typeof window !== "undefined"
+  ? createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  : null;
 
 export default function Home() {
   const [session, setSession] = useState(null);
